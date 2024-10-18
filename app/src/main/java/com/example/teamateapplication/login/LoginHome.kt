88acb07394721ac9b14830b3bedc3e4b889493f2
DaniPosse10 <daniel.possemato@gmail.com>
2024-10-18 12:16:41 +0200

    commit posse

--- app/src/main/java/com/example/teamateapplication/login/LoginHome.kt
+++ app/src/main/java/com/example/teamateapplication/login/LoginHome.kt
@@ -1,11 +1,11 @@
-package com.example.teamateapplication
+package com.example.teamateapplication.login
 
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.teamateapplication.R
 
 class LoginHome : AppCompatActivity() {
 
